a30193090a4c363327b0fedc82ce00f08718bf58
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-10 20:16:14 +0200

    se agrego boton de carrito de compras

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -20,6 +20,28 @@ const Producto = ({nvaGuitarra}) => {
                 <h3>{nombre}</h3>
                 <p className={styles.descripcion}>{descripcion}</p>
                 <p className={styles.precio}>${precio}</p>
+
+                <form className={styles.formulario}>
+                    <label>Cantidad:</label>
+
+                    <select>
+                        <option value=''>-- Seleccione --</option>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                        <option value='6'>6</option>
+                        <option value='7'>7</option>
+                        <option value='8'>8</option>
+                        <option value='9'>9</option>
+                    </select>
+
+                    <input
+                        type='submit' 
+                        value='Agregar al Carrito'
+                    />
+                </form>
             </div>
         </div>
     </Layout> 
