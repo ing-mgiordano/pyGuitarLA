9799101f713331af7130fe544fa05e5c8736defb
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-13 12:40:41 +0200

    se crea contenido de curso y se aplica css

--- components/Curso.js
+++ components/Curso.js
@@ -1,4 +1,4 @@
-
+import styles from "../styles/Curso.module.css"
 
 const Curso = ({curso}) => {
 
@@ -8,14 +8,26 @@ const Curso = ({curso}) => {
 
     return (
         <section>
-            <div className='contenedor'>
-                <div>
-                    <h2>{titulo}</h2>
-                    <p>{contenido}</p>
+            <div className={`contenedor ${styles.grid}`}>
+                <div className={styles.contenido}>
+                    <h2 className='heading'>{titulo}</h2>
+                    <p className={styles.texto}>{contenido}</p>
 
-                    <a href='#'>Mas Información</a>
+                    <a className={styles.enlace} href='#'>Mas Información</a>
                 </div>
             </div>
+
+            {/* cuando queremos pasar resultados de una api a css debemos usar esta sintaxis */}
+            <style jsx>{`
+                section {
+                    padding: 10rem 0;
+                    margin-top: 10rem;
+                    background-image: linear-gradient(to right, rgb(0 0 0 / .65), rgb(0 0 0 / .7)), url(${urlImagen});
+                    background-size: cover;
+                    background-position: 50%;
+                }
+               
+            `}</style>
         </section>
     )
 }
